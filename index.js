301da5b84e49163ab9e50ab2d2ac7efa5b0de664
Rafael-Ferreira-RJ <113391188+Rafael-Ferreira-RJ@users.noreply.github.com>
2023-01-31 18:43:50 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express') // importa o express
-const app = express() // app recebe tudo da função express
-const port = 3000 // definindo a porta do servidor que receberá as requisições
+const express = require('express')
+const app = express()
+const port = 3000
 
-//Definindo a Rota '/' para mostrar a mensagem Olá Mundo
-app.get('/', (req, res) => {
-  res.send('Olá Mundo!')
+app.get('/usuario', (req, res) => {
+  res.send('Rota ativada com GET e recurso usuário:nome, email e senha devem ser retornados')
 })
 
-// O servidor está escutando na porta 3000. Subiu o servidor
+app.get('/tarefas', (req, res) => {
+    res.send('Rota ativada com GET e recurso tarefas: título, data, status e descrição devem ser retornados')
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
