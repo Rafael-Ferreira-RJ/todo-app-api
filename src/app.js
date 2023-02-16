b3dbf796eb52813439e69fc984697239a665031b
Rafael-Ferreira-RJ <113391188+Rafael-Ferreira-RJ@users.noreply.github.com>
2023-02-16 19:41:07 -0300

    Atualização src

---
+++ src/app.js
@@ -0,0 +1,26 @@
+// Importando o packages
+const express = require('express')
+
+// Importa o cors
+const cors = require('cors')
+
+// instanciando o servidor
+const app = express()
+
+// configurando o servidor para receber requisições com o corpo no formato JSON
+app.use(express.json())
+
+// Cross origin recurse sharing - compartilhamento recursivo de origem cruzada "Diferentes" 
+// Usado para aceitar requisições vindas de origens diferentes, ou seja, qualquer outro http .... diferente do nosso.
+app.use(cors())
+
+// importando os controllers
+const usuarioController = require('./controllers/usuario-controller.js')
+const tarefaController = require('./controllers/tarefa-controller.js')
+
+// Chama as rotas
+usuarioController.rotas(app)
+tarefaController.rotas(app)
+
+// Exporta o app para ser usado em outro módulo, por exemplo: no server.js
+module.exports = app
